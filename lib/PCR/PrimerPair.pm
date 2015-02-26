6e87bd5e06ec601b2382eb38ec7d8c3bad870971
Richard White <richard.white@sanger.ac.uk>
2015-02-26 11:48:23 +0000

    Changes to PrimerPair
    lib/PCR/PrimerPair.pm
    Removed attribute type. This will now need to be included in a subclass if it is needed.
    Corrected Documentation.
    
    t/primer_pair.t
    Removed method type and test for type
    Added tests for primer_pair_summary and primer_pair_info methods.
    Commented out mocked methods that are not used at the moment.

--- lib/PCR/PrimerPair.pm
+++ lib/PCR/PrimerPair.pm
@@ -7,28 +7,6 @@ package PCR::PrimerPair;
 use PCR::Primer;
 use namespace::autoclean;
 use Moose;
-use Moose::Util::TypeConstraints;
-
-my %types = (
-	ext => 1,
-	int => 1,
-    illumina => 1,
-    illumina_tailed => 1,
-	hrm => 1,
-	flag => 1,
-	flag_revcom => 1,
-	ha => 1,
-	ha_revcom => 1,
-);
-my $error_message = "PrimerPair:Type attribute must be one of " . join(q{, }, sort keys %types) . ".\n";
-
-subtype 'PCR::PrimerPair::Type',
-	as 'Str',
-	where {
-		my $ok = 0;
-		$ok = 1 if( exists $types{ lc($_) } );
-	},
-	message { return $error_message; };
 
 =method new
 
@@ -42,7 +20,6 @@ subtype 'PCR::PrimerPair::Type',
                     'product_size' => '668',
                     'left_primer' => $left_primer,
                     'right_primer' => $right_primer,
-                    'type' => 'ext',
                     'pair_compl_end' => '0.00',
                     'pair_compl_any' => '3.00',
                     'pair_penalty' => '0.1303'
@@ -58,7 +35,6 @@ subtype 'PCR::PrimerPair::Type',
                 product_size        => Int
                 left_primer         => PCR::Primer
                 right_primer        => PCR::Primer
-                type                => Str
                 pair_compl_end      => Num
                 pair_compl_any      => Num
                 pair_penalty        => Num
@@ -83,8 +59,6 @@ has 'pair_name' => (
 	isa => 'Str',
 );
 
-=cut
-
 =method amplicon_name
 
   Usage       : $primer->amplicon_name;
@@ -93,8 +67,6 @@ has 'pair_name' => (
   Parameters  : None
   Throws      : If input is given
   Comments    : Can be undef
-
-=cut
 
 =cut
 
@@ -109,8 +81,6 @@ has 'pair_name' => (
 
 =cut
 
-=cut
-
 =method target
 
   Usage       : $primer->target;
@@ -119,8 +89,6 @@ has 'pair_name' => (
   Parameters  : None
   Throws      : If input is given
   Comments    : 
-
-=cut
 
 =cut
 
@@ -135,7 +103,6 @@ has 'pair_name' => (
 
 =cut
 
-=cut
 
 =method product_size_range
 
@@ -153,8 +120,6 @@ has [ 'amplicon_name', 'warnings', 'target', 'explain', 'product_size_range' ] =
 	isa => 'Str',
 );
 
-=cut
-
 =method excluded_regions
 
   Usage       : $primer->excluded_regions;
@@ -171,8 +136,6 @@ has 'excluded_regions' => (
 	isa => 'ArrayRef',
 );
 
-=cut
-
 =method product_size
 
   Usage       : $primer->product_size;
@@ -184,8 +147,6 @@ has 'excluded_regions' => (
 
 =cut
 
-=cut
-
 =method query_slice_start
 
   Usage       : $primer->query_slice_start;
@@ -194,8 +155,6 @@ has 'excluded_regions' => (
   Parameters  : None
   Throws      : If input is given
   Comments    : 
-
-=cut
 
 =cut
 
@@ -214,8 +173,6 @@ has [ 'product_size', 'query_slice_start', 'query_slice_end' ] => (
 	is => 'ro',
 	isa => 'Int',
 );
-
-=cut
 
 =method left_primer
 
@@ -236,8 +193,6 @@ has 'left_primer' => (
 	}
 );
 
-=cut
-
 =method right_primer
 
   Usage       : $primer->right_primer;
@@ -257,26 +212,6 @@ has 'right_primer' => (
 	}
 );
 
-=cut
-
-=method type
-
-  Usage       : $primer->type;
-  Purpose     : Getter for type attribute
-  Returns     : Str
-  Parameters  : None
-  Throws      : If input is given
-  Comments    : Must be one of 	ext, int, illumina, illumina_tailed, hrm, flag, flag_revcom, ha, OR ha_revcom
-
-=cut
-
-has 'type' => (
-	is => 'rw',
-	isa => 'PCR::PrimerPair::Type',
-);
-
-=cut
-
 =method pair_compl_end
 
   Usage       : $primer->pair_compl_end;
@@ -288,8 +223,6 @@ has 'type' => (
 
 =cut
 
-=cut
-
 =method pair_compl_any
 
   Usage       : $primer->pair_compl_any;
@@ -298,8 +231,6 @@ has 'type' => (
   Parameters  : None
   Throws      : If input is given
   Comments    : 
-
-=cut
 
 =cut
 
@@ -318,8 +249,6 @@ has [ 'pair_compl_end', 'pair_compl_any', 'pair_penalty' ] => (
 	is => 'ro',
 	isa => 'Num',
 );
-
-=cut
 
 =method primer_pair_summary
 
@@ -350,7 +279,7 @@ sub primer_pair_summary {
 
   Usage       : $primer->primer_pair_info;
   Purpose     : Returns Information about the primer pair
-                Amplicon Name, Pair Name, Type, Product Size,
+                Amplicon Name, Pair Name, Product Size,
                 Left Primer Info, Right Primer Info
   Returns     : Array
   Parameters  : None
@@ -364,7 +293,6 @@ sub primer_pair_info {
     my @info = (
 		$self->amplicon_name,
 		$self->pair_name,
-		$self->type,
 		$self->product_size,
     );
 	push @info, $self->left_primer->primer_info;
@@ -389,17 +317,18 @@ PCR::PrimerPair - Object representing a PCR primer pair.
  
     use <PCR::Primer_pair>;
     my $primer_pair = PCR::Primer_pair->new(
-    	target_id => 32,
-        talen_pair_id => 125,
-        type => 'ext',
-        left_primer => $left_primer,
-        right_primer => $right_primer,
-        left_primer_id => 1,
-        right_primer_id => 2,
-        plate_id => '001',
-        product_size => 200,
-        amplicon_name => 'ENSE02377374',
-        primer_pair_adaptor => $mock_primer_ad,
+        'pair_name' => '5:12345152-12345819:1:1',
+        'amplicon_name' => 'crRNA:5:12345678-12345700:1',
+        'target' => '701,23',
+        'explain' => 'considered 37, unacceptable product size 26, ok 11',
+        'product_size_range' => '500-1000',
+        'excluded_regions' => [ '651,122', '1282,29', ]
+        'product_size' => '668',
+        'left_primer' => $left_primer,
+        'right_primer' => $right_primer,
+        'pair_compl_end' => '0.00',
+        'pair_compl_any' => '3.00',
+        'pair_penalty' => '0.1303'
     );
     
   
--- t/primer_pair.t
+++ t/primer_pair.t
@@ -6,25 +6,29 @@ use Test::More;
 use Test::Exception;
 use Test::MockObject;
 
-plan tests => 1 + 18 + 1;
+plan tests => 1 + 17 + 2;
 
 use PCR::PrimerPair;
 
 # make 2 mock Primer objects
 my $mock_l_primer = Test::MockObject->new();
-$mock_l_primer->mock('seq_region', sub { return '5'} );
-$mock_l_primer->mock('seq_region_start', sub { return 2403050 } );
-$mock_l_primer->mock('seq_region_end', sub { return 2403073 } );
-$mock_l_primer->mock('seq_region_strand', sub { return '1' } );
-$mock_l_primer->mock('sequence', sub { return 'ACGATGACAGATAGACAGAAGTCG' } );
+#$mock_l_primer->mock('seq_region', sub { return '5'} );
+#$mock_l_primer->mock('seq_region_start', sub { return 2403050 } );
+#$mock_l_primer->mock('seq_region_end', sub { return 2403073 } );
+#$mock_l_primer->mock('seq_region_strand', sub { return '1' } );
+#$mock_l_primer->mock('sequence', sub { return 'ACGATGACAGATAGACAGAAGTCG' } );
+$mock_l_primer->mock('primer_summary', sub { return ( '5:2403050-2403073:1', 'ACGATGACAGATAGACAGAAGTCG' ) } );
+$mock_l_primer->mock('primer_info', sub { return ( '5:2403050-2403073:1', 'AGATAGACTAGACATTCAGATCAG', 24, 58.23, 45.5 ) } );
 $mock_l_primer->set_isa('PCR::Primer');
 
 my $mock_r_primer = Test::MockObject->new();
-$mock_r_primer->mock('seq_region', sub { return '5'} );
-$mock_r_primer->mock('seq_region_start', sub { return 2403250 } );
-$mock_r_primer->mock('seq_region_end', sub { return 2403273 } );
-$mock_r_primer->mock('seq_region_strand', sub { return '-1' } );
-$mock_r_primer->mock('sequence', sub { return 'AGATAGACTAGACATTCAGATCAG' } );
+#$mock_r_primer->mock('seq_region', sub { return '5'} );
+#$mock_r_primer->mock('seq_region_start', sub { return 2403250 } );
+#$mock_r_primer->mock('seq_region_end', sub { return 2403273 } );
+#$mock_r_primer->mock('seq_region_strand', sub { return '-1' } );
+#$mock_r_primer->mock('sequence', sub { return 'AGATAGACTAGACATTCAGATCAG' } );
+$mock_r_primer->mock('primer_summary', sub { return ( '5:2403050-2403273:-1', 'AGATAGACTAGACATTCAGATCAG' ) } );
+$mock_r_primer->mock('primer_info', sub { return ( '5:2403050-2403273:-1', 'AGATAGACTAGACATTCAGATCAG', 24, 58.23, 45.5 ) } );
 $mock_r_primer->set_isa('PCR::Primer');
 
 # make a new primer object
@@ -34,24 +38,29 @@ my $primer_pair = PCR::PrimerPair->new(
     product_size => '224',
     left_primer => $mock_l_primer,
     right_primer => $mock_r_primer,
-    type => 'ext',
 );
 
 # 1 test
 isa_ok( $primer_pair, 'PCR::PrimerPair');
 
-# test methods - 18 tests
+# test methods - 17 tests
 my @methods = qw(  pair_name amplicon_name warnings target explain
     product_size_range excluded_regions product_size query_slice_start query_slice_end
-    left_primer right_primer type pair_compl_end pair_compl_any
-    pair_penalty primer_pair_summary primer_pair_info
+    left_primer right_primer pair_compl_end pair_compl_any pair_penalty
+    primer_pair_summary primer_pair_info
 );
 
 foreach my $method ( @methods ) {
     can_ok( $primer_pair, $method );
 }
 
-# check type constraints - 1 tests
-throws_ok { PCR::PrimerPair->new( type => 'nt' ) }
-    qr/PrimerPair:Type/ms, 'wrong pair type';
+# check summary and info methods - 2 tests
+is( join(",", $primer_pair->primer_pair_summary),
+    'ENSDARE00000001,224,5:2403050-2403073:1,ACGATGACAGATAGACAGAAGTCG,5:2403050-2403273:-1,AGATAGACTAGACATTCAGATCAG',
+    'check primer primer_summary' );
+
+is( join(",", $primer_pair->primer_pair_info),
+   'ENSDARE00000001,5:2403050-2403273,224,5:2403050-2403073:1,AGATAGACTAGACATTCAGATCAG,24,58.23,45.5,5:2403050-2403273:-1,AGATAGACTAGACATTCAGATCAG,24,58.23,45.5',
+   'check primer_pair_info' );
+
 
